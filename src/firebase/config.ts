e42f65aed3ffec7e786a43c684a76e33ef9303e4
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-10-11 21:59:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-596135127-1b53f",
+  "appId": "1:122690202213:web:e935634613bb8780ccf48c",
+  "apiKey": "AIzaSyCs4msIIap62vnWD-QrYZXbEORIQZR0DM0",
+  "authDomain": "studio-596135127-1b53f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "122690202213"
+};
